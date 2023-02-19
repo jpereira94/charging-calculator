81cccc5edd579801986f81adc4858064e7e71db1
João Pereira <joao.pereira@afteryou.pt>
2023-02-19 22:16:12 +0000

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,38 @@
   <el-container>
     <el-main>
       <h1>EV Charging Simulator Portugal</h1>
-      <pre>{{ locations }}</pre>
+
+      <el-table
+        v-loading="$fetch.pending"
+        :data="displayData"
+        style="width: 100%"
+      >
+        <el-table-column
+          v-for="header in headers"
+          :key="header"
+          :prop="header"
+          :label="header"
+        >
+        </el-table-column>
+      </el-table>
+
+      <el-pagination
+        layout="prev, pager, next"
+        :current-page.sync="page"
+        :total="locations.length"
+        :page-size="pageSize"
+      >
+      </el-pagination>
+
+      <!-- <pre>{{ locations }}</pre> -->
     </el-main>
   </el-container>
 </template>
 
 <script>
 import Vue from 'vue'
+// eslint-disable-next-line no-unused-vars
+import Papa from 'papaparse'
 
 export default Vue.extend({
   name: 'IndexPage',
@@ -16,40 +41,39 @@ export default Vue.extend({
   data() {
     return {
       locations: [],
+      page: 1,
+      pageSize: 20,
+      userLocation: {},
     }
   },
   async fetch() {
     const response = await this.$axios.$get('/api/count')
     this.locations = response
 
-    // for (let i = 0; i < response.length; i++) {
-    //   const posto = response[i]
-    //   for (let j = 0; j < posto.evses.length; j++) {
-    //     const evse = posto.evses[j]
-    //     this.locations.push({
-    //       id: posto.id,
-    //       uid: evse.uid,
-    //       country_code: posto.country_code,
-    //       party_id: posto.party_id,
-    //       address: posto.address,
-    //       city: posto.city,
-    //       country: posto.country,
-    //       postal_code: posto.postal_code,
-    //       coordinates_latitude: posto.coordinates.latitude,
-    //       coordinates_longitude: posto.coordinates.longitude,
-    //       parking_type: posto.parking_type,
-    //       standard: evse.connectors[0].standard,
-    //       format: evse.connectors[0].format,
-    //       power_type: evse.connectors[0].power_type,
-    //       max_voltage: evse.connectors[0].max_voltage,
-    //       max_amperage: evse.connectors[0].max_amperage,
-    //       max_electric_power: evse.connectors[0].max_electric_power,
-    //       mobie_voltage_level: posto.mobie_voltage_level,
-    //     })
-    //   }
-    // }
+    // const results = await new Promise((resolve, reject) => {
+    //   Papa.parse('https://www.mobie.pt/documents/42032/106470/Tarifas', {
+    //     download: true,
+    //     header: true,
+    //     dynamicTyping: true,
+    //     complete: resolve,
+    //     error: reject,
+    //   })
+    // })
+    // this.locations = results.data
 
-    // this.locations = response
+    // console.log(results)
+  },
+  computed: {
+    displayData() {
+      return this.locations.slice(
+        this.pageSize * (this.page - 1),
+        this.pageSize * this.page
+      )
+    },
+    headers() {
+      if (this.locations.length === 0) return []
+      return Object.keys(this.locations[0])
+    },
   },
   // methods: {
   //   async getCount() {
@@ -59,6 +83,15 @@ export default Vue.extend({
   //   },
   // },
   mounted() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.userLocation.latitude = position.coords.latitude
+        this.userLocation.longitude = position.coords.longitude
+      },
+      (error) => {
+        console.error(error.message)
+      }
+    )
     // this.$notify({
     //   title: 'It works!',
     //   type: 'success',
