345a2264f6d15e18a699327e0b3d20a4c4e365bc
João Pereira <japereira@ualg.pt>
2023-02-20 15:16:10 +0000

    Add map

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,10 +21,14 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: ['element-ui/lib/theme-chalk/index.css', 'leaflet/dist/leaflet.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/element-ui'],
+  plugins: [
+    '@/plugins/element-ui',
+    '@/plugins/utils.js',
+    { src: '~plugins/leaflet.js', ssr: false },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,10 @@
   <el-container>
     <el-main>
       <h1>EV Charging Simulator Portugal</h1>
+      <p>Só são listados os postos até 10km do utilizador</p>
 
-      <el-table
-        v-loading="$fetch.pending"
+      <!-- <el-table
+        v-loading="$fetchState.pending"
         :data="displayData"
         style="width: 100%"
       >
@@ -20,12 +21,25 @@
       <el-pagination
         layout="prev, pager, next"
         :current-page.sync="page"
-        :total="locations.length"
+        :total="closestStations.length"
         :page-size="pageSize"
       >
-      </el-pagination>
+      </el-pagination> -->
 
-      <!-- <pre>{{ locations }}</pre> -->
+      <l-map :zoom="zoom" :center="center">
+        <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+        <l-marker :lat-lng="markerLatLng"></l-marker>
+        <l-marker
+          v-for="station in closestStations"
+          :key="station.uid"
+          :lat-lng="[
+            station.coordinates_latitude,
+            station.coordinates_longitude,
+          ]"
+        ></l-marker>
+      </l-map>
+
+      <!-- <pre>{{ tarifas }}</pre> -->
     </el-main>
   </el-container>
 </template>
@@ -41,15 +55,60 @@ export default Vue.extend({
   data() {
     return {
       locations: [],
+      tarifas: {},
       page: 1,
       pageSize: 20,
       userLocation: {},
+
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [51.505, -0.159],
+      markerLatLng: [51.504, -0.159],
+      // withPopup: latLng(47.41322, -1.219482),
+      // withTooltip: latLng(47.41422, -1.250482),
+      // currentZoom: 11.5,
+      // currentCenter: latLng(47.41322, -1.219482),
+      // showParagraph: false,
+      // mapOptions: {
+      //   zoomSnap: 0.5,
+      // },
+      // showMap: true,
     }
   },
   async fetch() {
-    const response = await this.$axios.$get('/api/count')
-    this.locations = response
-
+    // const response = await this.$axios.$get('/api/count')
+    // this.locations = response
+    const { data } = await this.$axios.get(
+      'https://ocpi.mobinteli.com/2.2/locations'
+    )
+    for (let i = 0; i < data.length; i++) {
+      const posto = data[i]
+      for (let j = 0; j < posto.evses.length; j++) {
+        const evse = posto.evses[j]
+        this.locations.push({
+          id: posto.id,
+          uid: evse.uid,
+          country_code: posto.country_code,
+          party_id: posto.party_id,
+          address: posto.address,
+          city: posto.city,
+          country: posto.country,
+          postal_code: posto.postal_code,
+          coordinates_latitude: posto.coordinates.latitude,
+          coordinates_longitude: posto.coordinates.longitude,
+          parking_type: posto.parking_type,
+          standard: evse.connectors[0].standard,
+          format: evse.connectors[0].format,
+          power_type: evse.connectors[0].power_type,
+          max_voltage: evse.connectors[0].max_voltage,
+          max_amperage: evse.connectors[0].max_amperage,
+          max_electric_power: evse.connectors[0].max_electric_power,
+          mobie_voltage_level: posto.mobie_voltage_level,
+        })
+      }
+    }
     // const results = await new Promise((resolve, reject) => {
     //   Papa.parse('https://www.mobie.pt/documents/42032/106470/Tarifas', {
     //     download: true,
@@ -59,20 +118,48 @@ export default Vue.extend({
     //     error: reject,
     //   })
     // })
-    // this.locations = results.data
-
+    // this.tarifas = results.data.reduce((prev, curr) => {
+    //   ;(prev[curr.ChargingStation] || (prev[curr.ChargingStation] = [])).push(
+    //     curr
+    //   )
+    //   return prev
+    // }, {})
     // console.log(results)
   },
   computed: {
     displayData() {
-      return this.locations.slice(
+      return this.closestStations.slice(
         this.pageSize * (this.page - 1),
         this.pageSize * this.page
       )
     },
     headers() {
-      if (this.locations.length === 0) return []
-      return Object.keys(this.locations[0])
+      if (this.displayData.length === 0) return []
+      return Object.keys(this.displayData[0])
+    },
+    closestStations() {
+      return (
+        this.locations
+          // .filter((station) => station.id.includes('FAR'))
+          .filter((x, i, self) => i === self.findIndex((y) => x.id === y.id))
+          .map((station) => ({
+            ...station,
+            distance2user: this.$measure(
+              station.coordinates_latitude,
+              station.coordinates_longitude,
+              this.userLocation.latitude,
+              this.userLocation.longitude
+            ),
+          }))
+          .filter((station) => station.distance2user < 10000)
+        // .sort((a, b) => {
+        //   const distA = a.distance2user
+        //   const distB = b.distance2user
+        //   if (distA < distB) return -1
+        //   if (distA > distB) return 1
+        //   return 0
+        // })
+      )
     },
   },
   // methods: {
@@ -85,8 +172,13 @@ export default Vue.extend({
   mounted() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.userLocation.latitude = position.coords.latitude
-        this.userLocation.longitude = position.coords.longitude
+        this.$set(this.userLocation, 'latitude', position.coords.latitude)
+        this.$set(this.userLocation, 'longitude', position.coords.longitude)
+        this.center = this.markerLatLng = [
+          position.coords.latitude,
+          position.coords.longitude,
+        ]
+        console.log(this.userLocation)
       },
       (error) => {
         console.error(error.message)
@@ -108,5 +200,13 @@ body {
   font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui,
     helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial,
     sans-serif;
+}
+.el-main {
+  display: flex;
+  flex-direction: column;
+  height: 98vh;
+}
+.leaflet-container {
+  flex: 1;
 }
 </style>
